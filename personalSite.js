8ddf17658b0bc2b9079d60428a13d09c0235bdfb
Aram <shinoaanderson@gmail.com>
2025-11-25 10:34:50 +0330

    personalSite

---
+++ personalSite.js
@@ -0,0 +1,58 @@
+let loader = document.querySelector(".loader-site")
+let body = document.body
+
+
+    window.addEventListener("load", function(){
+
+setTimeout(function(){
+loader.classList.add("hide")
+}, 1000);
+
+body.classList.remove("hide")
+body.classList.add("visible")
+})
+
+
+let curour = document.querySelector(".cursour")
+
+document.addEventListener("mousemove" , function(event){
+    curour.style.left= event.clientX+ "px"
+    curour.style.top= event.clientY+ "px"
+})
+
+let btns=document.querySelectorAll("button")
+
+btns.forEach(function(btn){
+    btn.addEventListener("mouseenter" , function(){
+    curour.classList.add("cursor-hovered")
+    btn.addEventListener("mouseleave", function(){
+   curour.classList.remove("cursor-hovered")
+    })
+})
+})
+console.log(getComputedStyle(document.body).backgroundColor
+)
+
+let icons= document.querySelectorAll(".icons")
+let sections = document.querySelectorAll(".page-section")
+
+
+icons.forEach(function(icon , index){
+    icon.addEventListener("click", function(){
+        if(sections[index]){
+              sections[index].scrollIntoView({ behavior: "smooth" });
+        }
+    })
+})
+
+let items = document.querySelectorAll(".inf-tems")
+let sideLine = document.querySelector(".side-line")
+
+items.forEach(function(item , index){
+    item.addEventListener("mouseenter", function(){
+        let topside = index*75
+        sideLine.style.top= topside+ "px"
+    })
+})
+
+    
